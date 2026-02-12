9fd14f1d80c6b1d3b1a702e2bd65f02e5e5820d4
4n-ch4n <an7honycl06@gmail.com>
2026-02-12 00:39:56 -0500

    fix: Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://4n-ch4n.github.io",
+  base: "portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
